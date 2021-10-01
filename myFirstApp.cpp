23550b2e8d8fc0f964ec7a011ddce95c76e45915
Hilman Hazazi <hafizd.muhammad.kren.403@gmail.com>
2021-10-01 12:41:42 +0700

    first runable program

--- myFirstApp.cpp
+++ myFirstApp.cpp
@@ -3,8 +3,3 @@
 int main() {
     std::cout << "Hello, Hilman!\n"; //This will outputting the text of "Hello, Hilman!" with new line
 }
-
-int newParagraph() {
-    std::cout << "What do you need today?\n";
-}
-
