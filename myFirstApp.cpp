d2aafa75d628aab83f09e2eb124a6187b405165d
Hilman Hazazi <hafizd.muhammad.kren.403@gmail.com>
2021-10-01 10:08:23 +0700

    test adding new class.

--- myFirstApp.cpp
+++ myFirstApp.cpp
@@ -3,3 +3,8 @@
 int main() {
     std::cout << "Hello, Hilman!\n"; //This will outputting the text of "Hello, Hilman!" with new line
 }
+
+int newParagraph() {
+    std::cout << "What do you need today?\n";
+}
+
