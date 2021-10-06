3715b232f8a0e7ba342ce6ae5916e0796d3cacce
Hilman Hazazi <hafizd.muhammad.kren.403@gmail.com>
2021-10-07 04:29:58 +0700

    adding "identityProfile.cpp"

--- identityProfile.cpp
+++ identityProfile.cpp
@@ -2,7 +2,8 @@
 #include <climits>
 
 int main() {
-    int intMax = INTgit _MAX;
+    int intMax = INT_MAX;
     std::cout << intMax << std::endl;
     std::cout << intMax + 1 << std::endl;
+    
 }
