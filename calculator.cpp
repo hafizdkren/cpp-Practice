1287dd3784cb3d5940b8632190ae50ea5fedd1a2
Hilman Hazazi <hafizd.muhammad.kren.403@gmail.com>
2021-10-25 22:26:08 +0700

    reduce unneccesary thing

---
+++ calculator.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+    std::string numericalInput;
+
+    int main() {
+        std::cout << "Welcome to my Calculator! Please input your numerical input here:" << std::endl;
+
+        std::cin >> numericalInput;
+
+        
+    }
