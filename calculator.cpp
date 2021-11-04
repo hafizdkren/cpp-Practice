a04a174f64bfedf6ca6095aedba238c5d64d4844
Hilman Hazazi <hafizd.muhammad.kren.403@gmail.com>
2021-11-04 14:28:08 +0700

    Put new calculator program as final result.
    Finally finish using if else and switch on cpp,
    still learning how to auto enter for the setup.

--- calculator.cpp
+++ calculator.cpp
@@ -1,11 +1,53 @@
 #include <iostream>
 
-    std::string numericalInput;
+    int firstNumber, secondNumber;
+    int operationSelect;
 
     int main() {
-        std::cout << "Welcome to my Calculator! Please input your numerical input here:" << std::endl;
+        std::cout << "Welcome to my Calculator! Please input first number here:" << std::endl;
+        std::cin >> firstNumber;
 
-        std::cin >> numericalInput;
+        std::cout << "Please input second number here:" << std::endl;
+        std::cin >> secondNumber;
 
+        std::cout << "Select your operation" << std::endl;
+        std::cout << "1. Plus, 2. Minus, 3. Multiply, 4. Divide" << std::endl;
+        std::cin >> operationSelect;
+
+/*Trying to using the if else for temporary*/
+/*
+
+        if (operationSelect == 1) {
+            std::cout << firstNumber + secondNumber << std::endl;
+        } else if (operationSelect == 2) {
+            std::cout << firstNumber - secondNumber << std::endl;
+        } else if (operationSelect == 3) {
+            std::cout << firstNumber * secondNumber << std::endl;
+        } else if (operationSelect == 4) {
+            std::cout << firstNumber / secondNumber << std::endl;
+        } else {
+            std::cout << "We cannot do other than supplied operation" << std::endl;
+        }
+*/
+/* Switch mode */
         
+        switch(operationSelect) {
+        case 1:
+            std::cout << firstNumber + secondNumber << std::endl;
+            break;
+        case 2:
+            std::cout << firstNumber - secondNumber << std::endl;
+            break;
+        case 3:
+            std::cout << firstNumber * secondNumber << std::endl;
+            break;
+        case 4:
+            std::cout << firstNumber / secondNumber << std::endl;
+            break;
+        default:
+            std::cout << "We cannot do other than supplied operation" << std::endl;
+            break;
+        }
+
+        return 0;
     }
