54fc5423e3accc05a743191ce7755b52a5a5780f
Hilman Hazazi <hafizd.muhammad.kren.403@gmail.com>
2021-11-17 13:47:54 +0700

    Making GUI version of calculator
    I just trying to using glfw and imgui as tool to making gui version of
    my calculator.
    
    maybe should try TUI version.

--- calculator.cpp
+++ calculator.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+//#include <GLFW/glfw3.h>
+//#include <imgui.h>
 
-    int firstNumber, secondNumber, operationSelect, askContinue;
+    int firstNumber, secondNumber, resultNumber, operationSelect;
+	char askContinue;
 
 	void opening() {
 		static bool isExecuted = false;
@@ -10,6 +13,22 @@
 
 		std::cout << "Welcome to my Calculator! ";
 	}
+
+/*
+	void windowCreation() {
+		glfwInit();
+		
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+		GLFWwindow* window = glfwCreateWindow(600, 800, "Calculator", NULL, NULL);
+		
+		ImGui::InputTextWithHint("First number", "eg: 50", 2^256, void firstNumber);
+		ImGui::InputTextWithHint("Second number", "eg: 50", 2^256, void secondNumber);
+
+		ImGui::Text(resultNumber);
+
+		glfwDestroyWindow(window);
+	}
+*/
 
     int main() {
         opening();
@@ -45,13 +64,18 @@
         }
 
 		std::cout << "Did you want to using the calculator again?" << std::endl;
-		std::cout << "1/0" << std::endl;
+		std::cout << "Y/n" << std::endl;
 		std::cin >> askContinue;
 		switch (askContinue) {
-			case 1:
+			case 'Y':
 				return main();
 				break;
-			case 0:
+			case 'y':
+				return main();
+				break;
+			case 'N':
+				return 0;
+			case 'n':
 				return 0;
 		}
 
